789982bf6404871118da6fb9d839fc4f7c4aa1cc
KotAliaksandr <cot.aleks4ndr@yndex.by>
2021-07-15 13:06:45 +0300

    SignIn layout

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles/styles.scss';
+
+const modalSignInBtn = document.getElementById('modalSignInBtn');
+const modalWindowSignIn = document.getElementById('modalWindowSignIn');
+const closeWindowSignIn = document.getElementById('closeWindowSignIn');
+
+modalSignInBtn.onclick = () => {
+    modalWindowSignIn.style.display = 'block';
+};
+
+closeWindowSignIn.onclick = () => {
+    modalWindowSignIn.style.display = 'none';
+};
