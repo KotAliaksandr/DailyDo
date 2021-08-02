c1e6638bfb71684619ec055c62b1ed4cf5af2eb1
KotAliaksandr <cot.aleks4ndr@yndex.by>
2021-08-02 18:20:42 +0300

    now you can add categories

--- shared/validation.js
+++ shared/validation.js
@@ -3,3 +3,4 @@ import { REGEXP } from "./constants/regexp";
 export const passwordLengthValidation = password => password.match(REGEXP.PASSWORD_LENGTH);
 export const emailValidation = email => email.match(REGEXP.EMAIL);
 export const userNameValidation = userName => userName.match(REGEXP.USERNAME);
+export const inputCategoryValidation = inputCategoryValue => inputCategoryValue.match(REGEXP.INPUT_CATEGORY);
